b70685fe9a4a11b07c0fc703f9d8616f3813b3d5
Yurve <https://github.com/Yurve>
2023-02-16 01:50:47 +0900

    motion detect

---
+++ sdk/build/intermediates/cxx/Debug/252o3m3j/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\user\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\user\\AndroidStudioProjects\\Motion_OpenCV\\sdk\\.cxx\\Debug\\252o3m3j\\arm64-v8a" ^
+  opencv_jni_shared
